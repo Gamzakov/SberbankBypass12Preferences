6dd460a87052e1d87affac9a3f96ba68d3d8c854
Gamzakov <justgamzakov@gmail.com>
2019-12-23 03:05:42 +0300

    Initial Commit

---
+++ SberbankBypass12Preferences/SberbankBypass12PreferencesController.h
@@ -0,0 +1,23 @@
+//
+//  SberbankBypass12PreferencesController.h
+//  SberbankBypass12Preferences
+//
+//  Created by Алексей Осипов on 23.12.2019.
+//  Copyright (c) 2019 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <Preferences/Preferences.h>
+
+@interface SberbankBypass12PreferencesController : PSListController
+{
+}
+
+- (id)getValueForSpecifier:(PSSpecifier*)specifier;
+- (void)setValue:(id)value forSpecifier:(PSSpecifier*)specifier;
+- (void)followOnTwitter:(PSSpecifier*)specifier;
+- (void)visitWebSite:(PSSpecifier*)specifier;
+- (void)makeDonation:(PSSpecifier*)specifier;
+
+@end
